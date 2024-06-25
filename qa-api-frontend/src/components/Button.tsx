4a3fae3f2dd52334e651aae808a5aff4bc528c12
Pedro Monteiro <pedromont436@gmail.com>
2024-06-25 17:16:38 -0300

    criando componente button

---
+++ qa-api-frontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
+
+type ButtonTypes = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ children, ...props }: ButtonTypes) => {
+  return (
+    <button
+      {...props}
+      className="bg-amber-400 text-amber-800  rounded py-1 px-4 cursor-pointer hover:bg-amber-500 hover:text-amber-800 transition-all"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
